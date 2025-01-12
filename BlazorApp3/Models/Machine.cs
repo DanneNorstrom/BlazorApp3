4a21b05b3947dd2d063eb71949f8dd0f4780e6a0
dannor0608 <93254330+dannor0608@users.noreply.github.com>
2025-01-12 01:07:42 +0100

    GUID added

--- BlazorApp3/Models/Machine.cs
+++ BlazorApp3/Models/Machine.cs
@@ -2,7 +2,7 @@
 {
     public class Machine
     {
-        public int Id { get; set; }
+        public string Id { get; set; } = System.Guid.NewGuid().ToString();
         public string Name { get; set; } = string.Empty;
         public string Status { get; set; } = string.Empty;
         public string Datato { get; set; } = string.Empty;
